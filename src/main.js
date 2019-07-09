1f4e43930f1e4fb52a09facfc3f90d446dce4c45
lml6666 <1483557675@qq.com>
2019-07-09 16:54:55 +0800

    下载vue-router，创建并注册VueRouter并挂载到Vue实例上

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import App from './App.vue'
 // 导入 ElementUI 组件库和样式
 import ElementUI from 'element-ui'
 import '../node_modules/element-ui/lib/theme-chalk/index.css'
+// 导入 router
+import router from './router/index.js'
+
 // 注册ElementUI
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+// 导入Vue
+import Vue from 'vue'
+
+// 导入 vue-router
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+// 导出router
+export default router
