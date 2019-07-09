5fc82f125d4864f66d0fbe71d8359af9343c824c
lml6666 <1483557675@qq.com>
2019-07-09 16:35:52 +0800

    下载导入饿了么组件和样式，并进行注册

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入 ElementUI 组件库和样式
+import ElementUI from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+// 注册ElementUI
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
